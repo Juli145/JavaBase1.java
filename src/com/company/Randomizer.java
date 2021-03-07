90aa7a32478db1a67fba5dcecf5b6db74e106e55
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-03-07 13:40:49 +0200

    Added validation and unique number

--- src/com/company/Randomizer.java
+++ src/com/company/Randomizer.java
@@ -2,46 +2,97 @@ package com.company;
 import java.util.Scanner;
 
 public class Randomizer {
+    Scanner scanner = new Scanner(System.in);
 
-    public int min;
-    public int max;
-    boolean isWorking = true;
+    public int min, max, randomNumber, uniqueNumbers;;
+    public int[] array;
+    String command;
 
     public void start() {
         setMinMax();
         boolean isWorking = true;
         while(isWorking){
-            System.out.println("Choose command: generate, exit, help");
-            Scanner scanner = new Scanner(System.in);
-            String command = scanner.next();
-            if (command.equals("generate")) {
-                int[] array = new int[max - min + 1];
-                int index = 0;
-                System.out.println("min number = " + min + "; max number = " + max);
-                int randomNumber = (int) (min + (Math.random() * max));
-                array[index++] = randomNumber;
-                System.out.println("random number = " + randomNumber);
-            } else if (command.equals("exit")) {
-                System.out.println("Bye-bye!");
-                isWorking = false;
-                System.exit(0);
-            } else if (command.equals("help")) {
-                System.out.println("Command 'generate' - generating random number");
-                System.out.println("Command 'exit' - quit app");
-            } else {
-                System.out.println("Wrong command. Please try again");
+            switch(getCommand()) {
+                case "generate":
+                    do {
+                        generateRandomNumber();
+                    } while(!isUniqueRandomNumber() && uniqueNumbers != max - min + 1);
+                    System.out.println(randomNumber);
+                    if(uniqueNumbers == max - min + 1) {
+                        System.out.println("There is no more numbers in your range. " +
+                                "Make one more try? (yes or no)");
+                        command = scanner.next();
+                        if(command.equals("no")) {
+                            System.out.println("bye bye!");
+                            System.exit(0);
+                        } else if(command.equals("yes")) {
+                            reset();
+                            setMinMax();
+                        }
+                    }
+                    break;
+                case "exit":
+                    System.out.println("Bye-bye!");
+                    isWorking = false;
+                    System.exit(0);
+                case "help":
+                    System.out.println("Command 'generate' - generating random number");
+                    System.out.println("Command 'exit' - quit app");
+                    getCommand();
+                default:
+                    System.out.println("Wrong command. Please try again");
             }
         }
     }
     public void setMinMax(){
-        Scanner scanner =new Scanner(System.in);
         System.out.println("Enter min number:");
-        min = scanner.nextInt();
+        min = getValidLimit();
         System.out.println("Enter max number:");
-        max = scanner.nextInt();
-        if (min <= 1 || max >= 500) {
-            System.out.println("Min number should be more than 0. Max number should be equal or less than 500 ");
-        }
+        max = getValidLimit();
+        System.out.println("min number = " + min + "; max number = " + max);
+        array = new int[max - min + 1];
     }
 
+    private int getValidLimit() {
+        int limit = 0;
+        boolean isValidLimit = false;
+        while(!isValidLimit) {
+            while(!scanner.hasNextInt()) {
+                scanner.next();
+                System.out.println("Floating point numbers are not allowed. Try again");
+            }
+            limit = scanner.nextInt();
+            if (limit < 1 ) {
+                System.out.println("The limit can not be less than 1");
+            } else if(limit > 500) {
+                System.out.println("The limit can not be more than 500");
+            } else {
+                isValidLimit = true;
+            }
+        }
+        return limit;
+    }
+
+    public void reset() {
+        uniqueNumbers = 0;
+    }
+    public void generateRandomNumber(){
+        randomNumber = min + (int)(Math.random() * (max - min + 1));
+    }
+    public boolean isUniqueRandomNumber() {
+        boolean isUnique = true;
+        for(int arraySecond : array) {
+            if(randomNumber == arraySecond) {
+                return false;
+            }
+        }
+        array[uniqueNumbers++] = randomNumber;
+        return isUnique;
+    }
+    public String getCommand(){
+        System.out.println("Choose command: generate, exit, help");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.next();
+        return command;
+    }
 }
